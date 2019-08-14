62468fef973fac78da9550fe6da0fcec71ed915d
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-08-14 11:41:00 -0400

    Action crators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,19 +3,42 @@
  * al ser ejecutada por primera vez se inicializa con initialState
  */
 
+//tipo de acciones, convención de nombre para evitar colisiones modulo/accion
+
+const INCREMENTAR   = 'CONTADOR/INCREMENTAR'
+const DECREMENTAR   = 'CONTADOR/DECREMENTAR'
+const SETEAR        = 'CONTADOR/SETEAR'
+/**
+ * retorna un obj con la propiedad de type
+ * acciones a despachar convencion de nombre minusculas
+ */
+const incrementar = () => ({
+    type: INCREMENTAR,
+})
+const decrementar = () => ({
+    type: DECREMENTAR,
+})
+/**
+ * función que setea el Nº que deseamos, recibe un payload
+ * si es que se desea adjuntar datos en las aciones a despachar se agrega payload
+ */
+//para setear el numero que queramos,
+const setear = (payload) => ({
+    type: SETEAR,
+    payload,
+})
 const initialState = 0
-
 export default function (state = initialState, action) {
-    
     console.log(action)
-
     switch (action.type) {
-        case 'INCREMENTAR':
+        case INCREMENTAR:
             return state + 1
 
-        case 'DECREMENTAR':
+        case DECREMENTAR:
             return state - 1
 
+        case SETEAR:
+            return action.payload
         default:
             return state
     }
